26d9c90f3730cf86667295387d7e85434e392aff
JimmyZhangJW <z1771485029@gmail.com>
2017-03-26 09:56:53 -0700

    final

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');
 var googleAuth = require('google-auth-library');
+var moment = require('moment');
+moment().format('YYYY-MM-DDTHH:mm:SS-07:00');
+
+var summary;
+var location;
+var startDTime;
+var endDTime;
+var description;
+var length;
+var count;
+var globalAuth;
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/calendar-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+var SCOPES = ['https://www.googleapis.com/auth/calendar'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
   process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
@@ -24,6 +35,7 @@ function callGCal() {
 	authorize(JSON.parse(content), listEvents);
 	});
 }
+callGCal();
 
 
 /**
@@ -46,6 +58,7 @@ fs.readFile(TOKEN_PATH, function(err, token) {
     getNewToken(oauth2Client, callback);
   } else {
     oauth2Client.credentials = JSON.parse(token);
+    globalAuth = oauth2Client;
     callback(oauth2Client);
   }
 });
@@ -78,6 +91,7 @@ rl.question('Enter the code from that page here: ', function(code) {
     }
     oauth2Client.credentials = token;
     storeToken(token);
+    globalAuth = oauth2Client;
     callback(oauth2Client);
   });
 });
@@ -100,18 +114,20 @@ fs.writeFile(TOKEN_PATH, JSON.stringify(token));
 console.log('Token stored to ' + TOKEN_PATH);
 }
 
+
 /**
 * Lists the next 10 events on the user's primary calendar.
 *
 * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
 */
 function listEvents(auth) {
+
 var calendar = google.calendar('v3');
 calendar.events.list({
   auth: auth,
   calendarId: 'primary',
   timeMin: (new Date()).toISOString(),
-  maxResults: 10,
+  maxResults: 100,
   singleEvents: true,
   orderBy: 'startTime'
 }, function(err, response) {
@@ -132,6 +148,39 @@ calendar.events.list({
   }
 });
 }
+
+function createEvent(auth, summary, loc, startTime, endTime) {
+    var calendar = google.calendar('v3');
+    var event = {
+        'summary': summary,
+        'location': loc,
+        'description': 'descrip',
+        'start': {
+            'dateTime': startTime,
+            'timeZone': 'America/Vancouver',
+        },
+        'end': {
+            'dateTime': endTime,
+            'timeZone': 'America/Vancouver',
+        }
+    }
+    
+    calendar.events.insert({
+                           auth: auth,
+                           calendarId: 'primary',
+                           resource: event,
+                           }, function(err, event) {
+                           if (err) {
+                           console.log('There was an error contacting the Calendar service: ' + err);
+                           return;
+                           }
+                           console.log('Event created: %s', event.htmlLink);
+                           });
+}
+
+
+
+
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -148,6 +197,14 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
+
+var luisAppId = process.env.LuisAppId || 'b80cb378-370b-41f9-a8a0-16e9c2827699';
+var luisAPIKey = process.env.LuisAPIKey || '1942c522d7504febb12d7c282fcaeae6';
+const LuisModelUrl = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/b80cb378-370b-41f9-a8a0-16e9c2827699?subscription-key=ffd7bd18a5d847c4b088a7c65a5a29c7&timezoneOffset=0.0&verbose=true' || `https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/${luisAppId}?subscription-key=${luisAPIKey}&verbose=true`;
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.recognizer(new builder.LuisRecognizer(LuisModelUrl));
+
 server.post('/api/messages', connector.listen());
 server.get('/api/messages', function(req, res) {
 	res.send('hello world');
@@ -156,12 +213,103 @@ server.get('/api/messages', function(req, res) {
 //=========================================================
 // Bots Dialogs
 //=========================================================
+var time;
+var date;
+var givenTime;
+var eventName;
+var estimatedTime;
+var listofTimes;
 
-bot.dialog('/', function (session) {
-	callGCal();
-    session.send("Hello World");
-    // session.beginDialog('/calender')
-});
+
+bot.dialog('/', dialog);
+
+dialog.matches('Hello', [
+    function (session, args, next) {
+    session.send("Hi, I am the calender bot, want can I help you");}
+]);
+dialog.matches('None', [
+    function (session, args, next) {
+    session.send("Sorry, I don't understand, can you repeat it");}
+]);
+dialog.matches('Exercise', [
+    function (session, args, next) {
+
+    date=builder.EntityRecognizer.findEntity(args.entities,'builtin.datetime.date');
+    time=builder.EntityRecognizer.findEntity(args.entities,'builtin.datetime.time');
+    eventName = builder.EntityRecognizer.findEntity(args.entities,'Subject');
+    builder.Prompts.text(session, 'How many minutes do you want to exercise for?'); 
+   }, 
+     function (session, results) {
+      var duration = results.response;
+      console.log(date);
+      console.log(time);
+      console.log(eventName);
+
+      // session.send(duration);
+      if(time&&date) {
+      	if(date.entity.toLowerCase() =="tomorrow"){
+      		var extraday =1;
+      	}
+      	else {
+      		var extraday = 0;
+      	}
+    	createEvent(globalAuth, eventName.entity,"",moment(time).add(extraday,'days').format('YYYY-MM-DDTHH:mm:SS-07:00'), moment(time).add(extraday,'days').add(duration, 'minutes').format('YYYY-MM-DDTHH:mm:SS-07:00'));
+
+     }
+     // else if(date){
+     // 		//TODO:SEARCH
+     // 		var now = moment().format('YYYY-MM-DDTHH:mm:SS-07:00');
+     // 		var calendar = google.calendar('v3');
+     // 		var nextAvailTime = calendar.freebusy.query({
+     // 			auth: globalAuth,
+     // 			resource: {
+     // 				items: [{'id':0}],
+     // 			timeMin: moment().format('YYYY-MM-DDTHH:mm:SS-07:00'),
+     // 			timeMax: moment().add(5).format('YYYY-MM-DDTHH:mm:SS-07:00'),
+     // 			}}, function (err, res) {
+     // 				if (err) console.log(err);
+     // 				var events = res.calendars[0].busy;
+     // 				if (events.length == 0) {
+     // 					console.log('no upcoming events found');
+     // 				} else {
+     // 					console.log('busy here');
+     // 				}
+     // 			});
+
+
+     // }else{
+
+     // }
+    }
+
+    
+]);
+dialog.matches('Meeting', [
+    function (session, args, next) {
+    session.send("testing:Meeting");}
+]);
+dialog.matches('chores', [
+    function (session, args, next) {
+    session.send("testing:chores");}
+]);
+dialog.matches('haveHaircut', [
+    function (session, args, next) {
+    session.send("testing:haveHaircut");}
+]);
+dialog.matches('haveMeal', [
+    function (session, args, next) {
+    session.send("testing:meal");}
+]);
+dialog.matches('nap', [
+    function (session, args, next) {
+    session.send("testing:nap");}
+]);
+dialog.matches('shopping', [
+    function (session, args, next) {
+    session.send("testing:shopping");}
+]);
+
+
 
 bot.dialog('/calender', [
 	function (session) {
